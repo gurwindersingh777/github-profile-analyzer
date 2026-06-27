749a1a38f637d83b49f0a4633d814d6a47c2179e
Gurwinder Singh <singhgurwinder2004@gmail.com>
2026-06-27 12:04:15 +0530

    Add Providers component to RootLayout and enable client-side rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Providers from "@/components/provider";
 
 export const metadata: Metadata = {
   title: "Github Profile Analyzer",
@@ -8,7 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
